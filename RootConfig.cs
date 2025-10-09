fd85f9df01b792818b6ff3e7cd705b52600a383e
colin_greens <colin.j.benson@gmail.com>
2025-10-09 11:37:12 -0700

    first commit

---
+++ RootConfig.cs
@@ -0,0 +1,7 @@
+public record RootConfig
+{
+    public string Output_File { get; set; } = "";
+    public int Target_Frame_Width { get; set; }
+    public int Target_Frame_Height { get; set; }
+    public List<SpriteSheetInfo> Sheets { get; set; } = new();
+}
---
+++ SpriteSheetInfo.cs
@@ -0,0 +1,12 @@
+using System.Xml.Serialization;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+public record SpriteSheetInfo
+{
+    public string Path { get; set; } = "";
+    public int Frame_Width { get; set; }
+    public int Frame_Height { get; set; }
+    [XmlIgnore]
+    public List<Image<Rgba32>> ProcessedFrames { get; set; } = new();
+}
